0e434032a1ae303a5595597ce716ed3a09a0bcc7
CoalaCode <max.schneider@student.htw-berlin.de>
2024-02-14 23:17:54 +0100

    Earth rotation working

--- GeographyGame/Assets/Scripts/Game/SpeedManager.cs
+++ GeographyGame/Assets/Scripts/Game/SpeedManager.cs
@@ -1,97 +1,87 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SpeedManager : MonoBehaviour
 {
     [SerializeField] Vector3 ScaleDown = Vector3.one * 0.75f;
-    //[SerializeField] Image play;
-    //[SerializeField] Image pause;
+    [SerializeField] Vector3 ScaleUp = Vector3.one;
     [SerializeField] Button[] Buttons;
-    int lastid;
-    public enum Speed { Stop = 0, Normal = 1, Fast = 2, UltraFast = 3 }
-    //public Speed LastSpeed;
-    private Speed _CurrenSpeed;
-    public Speed CurrenSpeed
-    {
-        get => _CurrenSpeed;
-        set
-        {
-            //if (LastSpeed != value && value != Speed.Stop) LastSpeed = value;
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                if ((int)value == i) transform.GetChild(i).localScale = Vector3.one;
-                else transform.GetChild(i).localScale = ScaleDown;
+    [SerializeField] Transform earth;
 
-                if ((int)value == 0)
-                {
-                    Time.timeScale = 0;
-                }
-                if ((int)value == 1)
-                {
-                    Time.timeScale = 1;
-                }
-                if ((int)value == 2)
-                {
-                    Time.timeScale = 10;
-                }
-                if ((int)value == 3)
-                {
-                    Time.timeScale = 100;
-                }
-            }
-            Debug.Log("Speed Changed: " + value);
-            _CurrenSpeed = value;
-        }
-    }
-    public static SpeedManager instance;
+    public enum Speed { Stop = 0, Normal = 1, Fast = 2, UltraFast = 3 }
+    private Speed _currentSpeed;
+    private Coroutine rotationCoroutine;
+
     private void Start()
     {
-        instance = this;
         for (int i = 1; i < transform.childCount; i++)
         {
-            transform.GetChild(i).localScale = ScaleDown;
+            Buttons[i].transform.localScale = ScaleDown;
         }
 
-        CurrenSpeed = Speed.Normal;
-        Buttons[0].onClick.AddListener(ClickPause);
-        Buttons[1].onClick.AddListener(ClickPlay);
-        Buttons[2].onClick.AddListener(ClickFast);
-        Buttons[3].onClick.AddListener(ClickUltra);
+        SetSpeed(Speed.Normal);
+        Buttons[0].onClick.AddListener(() => SetSpeed(Speed.Stop));
+        Buttons[1].onClick.AddListener(() => SetSpeed(Speed.Normal));
+        Buttons[2].onClick.AddListener(() => SetSpeed(Speed.Fast));
+        Buttons[3].onClick.AddListener(() => SetSpeed(Speed.UltraFast));
     }
-
-    /*
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space)) 
-            if (Time.timeScale <1) CurrenSpeed = LastSpeed;
-            else
-            {
-
-                CurrenSpeed = Speed.Stop;
-            }
-
-        if (Input.GetKeyDown(KeyCode.Alpha1)) CurrenSpeed = Speed.Normal;
-        if (Input.GetKeyDown(KeyCode.Alpha2)) CurrenSpeed = Speed.Fast;
-        if (Input.GetKeyDown(KeyCode.Alpha3)) CurrenSpeed = Speed.UltraFast;
-    }
-    */
-    public void Click(int id)
-    {
-        CurrenSpeed = (Speed)id;
-    }
-    void ClickPause() { Click(0); }
-    void ClickPlay() { Click(1); }
-    void ClickFast() { Click(2); }
-    void ClickUltra() { Click(3); }
 
     private void OnDestroy()
     {
-        Buttons[0].onClick.RemoveListener(ClickPause);
-        Buttons[1].onClick.RemoveListener(ClickPlay);
-        Buttons[2].onClick.RemoveListener(ClickFast);
-        Buttons[3].onClick.RemoveListener(ClickUltra);
+        Buttons[0].onClick.RemoveAllListeners();
+        Buttons[1].onClick.RemoveAllListeners();
+        Buttons[2].onClick.RemoveAllListeners();
+        Buttons[3].onClick.RemoveAllListeners();
     }
 
+    private void SetSpeed(Speed speed)
+    {
+        if (_currentSpeed != speed)
+        {
+            // Reset scale of all buttons
+            foreach (Button button in Buttons)
+            {
+                button.transform.localScale = ScaleDown;
+            }
+
+            // Set scale of the button corresponding to the selected speed
+            Buttons[(int)speed].transform.localScale = ScaleUp;
+
+            _currentSpeed = speed;
+            UpdateRotation();
+        }
+    }
+
+    private void UpdateRotation()
+    {
+        if (rotationCoroutine != null)
+        {
+            StopCoroutine(rotationCoroutine);
+        }
+
+        switch (_currentSpeed)
+        {
+            case Speed.Stop:
+                break;
+            case Speed.Normal:
+                rotationCoroutine = StartCoroutine(RotateEarth(1f));
+                break;
+            case Speed.Fast:
+                rotationCoroutine = StartCoroutine(RotateEarth(10f));
+                break;
+            case Speed.UltraFast:
+                rotationCoroutine = StartCoroutine(RotateEarth(100f));
+                break;
+        }
+    }
+
+    private IEnumerator RotateEarth(float rotationSpeed)
+    {
+        while (true)
+        {
+            earth.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
 }
